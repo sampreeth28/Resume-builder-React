2ec02fde6f64d8a7cf5dbab008175429f3150f1a
sampreeth28 <sampreetkvinesh@gmail.com>
2025-11-13 10:21:25 +0530

    final commit

--- src/services/ServerURL.js
+++ src/services/ServerURL.js
@@ -1,3 +1,4 @@
-const ServerURL = "http://localhost:3000"
+// const ServerURL = "http://localhost:3000"
+const ServerURL = "https://resume-builder-server-p3nq.onrender.com"
 
 export default ServerURL
